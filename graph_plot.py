535033175618a542cc5c89eca96f38c3b9f4cc45
Devpratim Das <43448858+itsDevhere@users.noreply.github.com>
2019-05-19 12:10:18 +0530

    v4.1 Final Code upload
    Minor tweaks done

--- graph_plot.py
+++ graph_plot.py
@@ -2,38 +2,51 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotPieChart(positive, wpositive, spositive, negative, wnegative,snegative, neutral, searchTerm, noOfSearchTerms):
+def plotPieChart(positive, wpositive, spositive, negative, wnegative,snegative, neutral, searchTerm, noOfSearchTerms,hash_tag):
 #--Create pie chart--#
+    fig = plt.figure(figsize=(10, 4), dpi=130)
+    ax1 = fig.add_subplot(1, 2, 1, title="\nAnalyzing " + str(noOfSearchTerms) + " tweets on " + searchTerm)
+    ax2 = fig.add_subplot(1, 2, 2, title='Frequent hashtags from Tweets fetched.\n')
     labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]', 'Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
               'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
     sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
     colors = ['yellowgreen', 'lightgreen', 'darkgreen', 'gold', 'red', 'lightsalmon', 'darkred']
-    plt.pie(sizes, colors=colors, startangle=90)
-    plt.legend(labels, loc="best") #Changes to code from plt.legend(patches, labels, loc="best")
-    plt.title("\nAnalyzing " + str(noOfSearchTerms) + " tweets on " + searchTerm)
-    plt.axis('equal')
+    ax1.pie(sizes, colors=colors, startangle=90)
+    ax1.legend(labels, loc="best", fontsize=7 ) #Changes to code from plt.legend(patches, labels, loc="best")
+    #ax1.title("\nAnalyzing " + str(noOfSearchTerms) + " tweets on " + searchTerm)
+    ax1.axis('equal')
+    #ax1.tight_layout()
+    wc = WordCloud(width=1500, height=1200, background_color="white", relative_scaling=0.5).generate(hash_tag)
+    ax2.imshow(wc, interpolation='bilinear')
+    ax2.axis('off')
+    #ax2.title('Frequent hashtags from Tweets fetched.', color='black')
+    plt.show()
+
+def barGraphP(pos,neg,nums):
+    n = 8
+    X = np.arange(n)
+    #pos=[]
+    #neg=[]
+    #for name in files:
+        #pos.append(temp[0] + temp[1] + temp[2])
+        #neg.append(temp[3] + temp[4] + temp[5])
+        #nums.append(name + '\n' + 'with' +'\n' + str(temp[7]) + '\n' + 'tweets')    
+    YP = np.array(pos)
+    YN = np.array(neg)
+    #plt.axes([2, 2, 0.95, 0.95]) not supported with tight layout
+    plt.figure(figsize=(10, 5))
+    plt.bar(X, +YP, facecolor='#9999ff', edgecolor='white', label='positive tweets',width = 0.7)
+    plt.bar(X, -YN, facecolor='#ff9999', edgecolor='white', label='negative tweets',width = 0.7)
+    plt.legend(loc='upper right')
+    plt.ylim(-40,60)
+    for x, y in zip(X, YP):
+        plt.text(x , y + 0.05, '%.2f' % y + '%', ha='center', va= 'bottom')
+    for x, y in zip(X, YN):
+        plt.text(x , -y - 0.1, '%.2f' % y + '%', ha='center', va= 'top')
+    plt.title("\nAnalyzing positive and negative tweet percentage ")
+    plt.xticks(X, nums, fontsize=12)
     plt.tight_layout()
     plt.show()
-
-def barGraphP(npositivet,files):
-    objects=files
-    index=np.arange(len(objects))
-    plt.bar(index,npositivet,align="center",alpha=0.5,color=['mediumblue','darkorange','lightseagreen','red','green','lime','midnightblue','darkred'])
-    plt.ylabel("Percent of positive tweets")
-    plt.xlabel("Parties")
-    plt.xticks(index, objects)
-    plt.title("Positive percent of tweets per party")
-    plt.show()
-
-
-def cloud(hash):  # @ReservedAssignment
-#--Create word cloud--#    
-    wc = WordCloud(background_color="white", relative_scaling=0.5).generate(hash)
-    plt.imshow(wc, interpolation='bilinear')
-    plt.axis('off')
-    plt.title('Frequent hashtags from Tweets fetched.', color='black')
-    plt.show()
-
 
 def plottest(Gr,nodelist,mainnode):
     nodelist=list(dict.fromkeys(nodelist))
--- main.py
+++ main.py
@@ -13,29 +13,35 @@ class MainWindow(QtWidgets.QMainWindow):
     ntotaltweets = []
     nodelist=[]
     files = ['Congress','BJP' ,'TMC', 'CPIM','AAP','RJD','BSP','Samajwadi']
+    pos = []
+    neg = []
+    nums = []
     
     def __init__(self):
             
-        print("\nDownloading Data")
+        print("\n\tDownloading Data")
+
         for names in MainWindow.files:
             try:
                 te.downloadcsv(names)
             except:
                 pass
-        print("\nData Downloaded\nAnalyzing Data and Plotting Graphs")
+        print("\n\tData Downloaded\n\tAnalyzing Data and Plotting Graphs")
         for names in MainWindow.files:
             temp = te.readfromcsv(names)
-            gp.plotPieChart(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], names, temp[7])
-            gp.cloud(temp[8])
+            gp.plotPieChart(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], names, temp[7],temp[8])
+            #gp.cloud(temp[8])
             MainWindow.ntotaltweets.append(temp[7])
             MainWindow.npostweets.append(len(temp[9]))
+            MainWindow.pos.append(temp[0] + temp[1] + temp[2])
+            MainWindow.neg.append(temp[3] + temp[4] + temp[5])
+            MainWindow.nums.append(names + '\n' + 'with' +'\n' + str(temp[7]) + '\n' + 'tweets')
             MainWindow.H = gp.plottest(MainWindow.H, temp[9], names)
         
-        for i in range(len(MainWindow.npostweets)):
-            MainWindow.npostweets[i]=te.percentage(MainWindow.npostweets[i],MainWindow.ntotaltweets[i])
+        #for i in range(len(MainWindow.npostweets)):
+            #MainWindow.npostweets[i]=te.percentage(MainWindow.npostweets[i],MainWindow.ntotaltweets[i])
  
-        gp.barGraphP(MainWindow.npostweets,MainWindow.files)
-        
+        gp.barGraphP(MainWindow.pos,MainWindow.neg,MainWindow.nums)
         super(MainWindow,self).__init__() #This is the parent window object
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
@@ -66,17 +72,17 @@ class MainWindow(QtWidgets.QMainWindow):
             if v<10:
                 MainWindow.nodelist.append(v*50)
             else:
-                MainWindow.nodelist.append(1000)
-                
+                MainWindow.nodelist.append(500)
+
         nx.draw(G, node_size=MainWindow.nodelist, with_labels=True,alpha=0.8)
+        plt.savefig('Network Graph.png',dpi=200)
         plt.show()
         self.ui.progressBar.setValue(100)
         self.ui.pushButton.setDisabled(False)
-            
+        
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     w.show()
     sys.exit(app.exec_())
-
